553cadae690a5e63f30c38972d8dc569689e68da
suresh teli <suresh.teli@esq.com>
2022-08-05 11:47:09 +0530

    Added TS support in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "plugin:prettier/recommended",
+    "@vue/typescript",
   ],
+
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
   },
+
   overrides: [
     {
       files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
