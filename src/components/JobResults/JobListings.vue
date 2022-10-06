4e9ae67f1206d7a03abd05f7a07d8eb87951e32f
suresh teli <suresh.teli@esq.com>
2022-10-06 18:13:49 +0530

    Check for jobs count

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,31 +1,40 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
-    <ol>
-      <job-listing
-        v-for="job in displayedJobs"
-        :key="job.id"
-        :job="job"
-        data-test="job-listing"
-      />
-    </ol>
-    <div class="mt-8 mx-auto">
-      <div class="flex flex-row flex-nowrap">
-        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
-        <div class="flex items-center justify-center">
-          <router-link
-            v-if="previousPage"
-            :to="{ name: 'JobResults', query: { page: previousPage } }"
-            class="mx-3 text-sm font-semibold text-brand-blue-2"
-            data-test="previous-page-link"
-            >Previous</router-link
-          >
-          <router-link
-            v-if="nextPage"
-            :to="{ name: 'JobResults', query: { page: nextPage } }"
-            class="mx-3 text-sm font-semibold text-brand-blue-2"
-            data-test="next-page-link"
-            >Next</router-link
-          >
+    <div v-if="displayedJobs.length === 0">
+      <div class="self-center">
+        <span v-pre class="text-center font-semibold text-xl text-black"
+          >No Jobs Found. Please clear filter and try again.
+        </span>
+      </div>
+    </div>
+    <div v-else>
+      <ol>
+        <job-listing
+          v-for="job in displayedJobs"
+          :key="job.id"
+          :job="job"
+          data-test="job-listing"
+        />
+      </ol>
+      <div class="mt-8 mx-auto">
+        <div class="flex flex-row flex-nowrap">
+          <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+          <div class="flex items-center justify-center">
+            <router-link
+              v-if="previousPage"
+              :to="{ name: 'JobResults', query: { page: previousPage } }"
+              class="mx-3 text-sm font-semibold text-brand-blue-2"
+              data-test="previous-page-link"
+              >Previous</router-link
+            >
+            <router-link
+              v-if="nextPage"
+              :to="{ name: 'JobResults', query: { page: nextPage } }"
+              class="mx-3 text-sm font-semibold text-brand-blue-2"
+              data-test="next-page-link"
+              >Next</router-link
+            >
+          </div>
         </div>
       </div>
     </div>
